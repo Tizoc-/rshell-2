1125caa072ee97e46833827ce0cb6aff7b8a8af8
trevorsalazar <tsala001@ucr.edu>
2016-05-04 18:01:59 +0000

    setting up files for connector classes

---
+++ Oror.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <sstream>
+#include "Command.h"
+using namespace std;
+
+void Oror::execute()
+{
+    
+}
