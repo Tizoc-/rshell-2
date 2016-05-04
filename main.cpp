afb5af801613e977f377855116136e7b2369381e
trevorsalazar <tsala001@ucr.edu>
2016-05-04 18:04:40 +0000

    adding main file for program

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <sstream>
+#include "Base.h"
+#include "Command.h"
+#include "Processes.h"
+#include "Sentinel.h"
+#include "Andand.h"
+#include "Oror.h"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    return 0;
+}
