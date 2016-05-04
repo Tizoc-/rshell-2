4c32c3c66304113d1d8f22c851212dc2c4eccf2b
trevorsalazar <tsala001@ucr.edu>
2016-05-04 17:02:36 +0000

    first commit to command.cpp

---
+++ Command.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <sstream>
+#include "Command.h"
+using namespace std;
+
+Command::Command()
+{
+    command = "";
+    argument = "";
+}
+
+Command::Command(string comm, string arg)
+{
+    command = comm;
+    argument = arg;
+}
+
+string Command::setCommand(string comm)
+{
+    this->command = comm;
+}
+
+string Command::setArg(string arg)
+{
+    this->argument = arg;
+}
+
+void Command::execute()
+{
+    
+}
