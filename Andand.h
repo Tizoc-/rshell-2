44b95985a145b5ab1b0eae0504c60b2ae05d9863
trevorsalazar <tsala001@ucr.edu>
2016-05-04 17:59:09 +0000

    adding connector classes

---
+++ Andand.h
@@ -0,0 +1,9 @@
+#ifndef ANDAND_H
+#define ANDAND_H
+#include "Base.h"
+#include "Sentinel.h"
+class Andand: public Sentinel
+{
+    public:
+        void execute();
+}
---
+++ Oror.h
@@ -0,0 +1,9 @@
+#ifndef OROR_H
+#define OROR_H
+#include "Base.h"
+#include "Sentinel.h"
+class Oror: public Sentinel
+{
+    public:
+        void execute();
+}
