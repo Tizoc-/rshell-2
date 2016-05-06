6659ffe3d10a2e464d650ade3da881d44d921243
trevorsalazar <tsala001@ucr.edu>
2016-05-06 14:48:07 +0000

    testing ideas

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <sstream>
+#include <stdio.h>
+#include <unistd.h>
+#include "Base.h"
+#include "Command.h"
+#include "Processes.h"
+#include "Sentinel.h"
+#include "Andand.h"
+#include "Oror.h"
+using namespace std;
+
+int main()
+{
+    return 0;
+}
