03d259c1c053f525bf3f9fdbb061692c2e67e9a8
trevorsalazar <tsala001@ucr.edu>
2016-05-08 20:06:53 +0000

    made empty destructor for future classes

---
+++ Base.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <sstream>
+#include <stdio.h>
+#include <unistd.h>
+#include <cstring>
+#include <sys/wait.h>
+#include <algorithm>
+using namespace std;
+
+Base::~Base()
+{
+}
