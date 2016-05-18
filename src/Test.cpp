eff1dd2d1a4e1771dd5582736f2b218bb52d1ed2
trevorsalazar <tsala001@ucr.edu>
2016-05-18 17:43:55 +0000

    finished test

--- src/Test.cpp
+++ src/Test.cpp
@@ -8,8 +8,97 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <algorithm>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 using namespace std;
 #include "Command.h"
 #include "Test.h"
 
-Test::Test()
+Test::Test(vector<string> commandVec)
+{
+    this->cmdVec = commandVec;
+}
+
+int Test::getrunstat()
+{
+    return this->runStat;
+}
+
+int Test::execute()
+{
+    bool dashf = false;
+    bool dashd = false;
+    for(unsigned i = 0; i < cmdVec.size(); ++i)
+    {
+        if(cmdVec.at(i) == "-d")
+        {
+            dashd = true;
+        }
+        if(cmdVec.at(i) == "-f")
+        {
+            dashf = true;
+        }
+    }
+    runStat = 1;
+    char** cstrings = new char*[cmdVec.size() + 1];
+    unsigned i = 0;
+    for(; i < this->cmdVec.size(); ++i)
+    {
+        cstrings[i] = new char[cmdVec[i].size() + 1];
+        std::strcpy(cstrings[i], cmdVec[i].c_str());
+    }
+    cstrings[i] = NULL;
+    struct stat sb;
+    
+    if(!dashf && !dashd)
+    {
+        if (stat(cstrings[2], &sb) == -1) {
+            cout << "False" << endl;
+            runStat = -1;
+            return runStat;
+        }
+        cout << "True" << endl;
+        return runStat + 1;
+    }
+    else if(dashd)
+    {
+        if (stat(cstrings[2], &sb) == -1) {
+            cout << "False" << endl;
+            runStat = -1;
+            return runStat;
+        }
+        
+        switch (sb.st_mode & S_IFMT) {
+        case S_IFDIR:  dashd = false;                   break;
+        }
+        
+        if(!dashd)
+        {
+            cout << "True" << endl;
+            return runStat + 1;
+        }
+        cout << "False" << endl;
+        return runStat - 1;
+    }
+    else{
+        if (stat(cstrings[2], &sb) == -1) {
+            cout << "False" << endl;
+            runStat = -1;
+            return runStat;
+        }
+        switch (sb.st_mode & S_IFMT) {
+        case S_IFREG:  dashf = false;                    break;
+        }
+        
+        if(!dashf)
+        {
+            cout << "True" << endl;
+            return runStat + 1;
+        }
+        cout << "False" << endl;
+        return runStat - 1;
+    }
+    delete[] cstrings;
+    return runStat;
+}
--- src/Test.h
+++ src/Test.h
@@ -1,7 +1,10 @@
 #ifndef TEST_H
 #define TEST_H
 #include "Command.h"
-class Test : public Command {
+class Test : public Base {
+    private:
+        int runStat;
+        std::vector<std::string> cmdVec;
     public:
         Test(std::vector<std::string> commandVec);
         int execute();
